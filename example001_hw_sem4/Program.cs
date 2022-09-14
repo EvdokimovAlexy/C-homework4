5686105b150fe01cd35744f41bf1c055b6c8d862
Prometheus <poligraphich@gmail.com>
2022-09-14 21:41:58 +0300

    add exam001_s4

---
+++ example001_hw_sem4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+//  и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+Console.WriteLine("Input NumberA");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input NumberB");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+int Getexponentiate(int num1)
+{
+    int index = 0;
+    int count = 1;
+    int length = NumberB;
+    int temp = NumberA;
+    while (index < length)
+    {
+        count = count * NumberA;
+        index++;
+    }
+    return count;
+}
+int Number = Getexponentiate(NumberA);
+Console.WriteLine(Number);
