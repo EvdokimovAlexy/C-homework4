f4fd03ba0d60819e0c54f20030a543871637ea91
Prometheus <poligraphich@gmail.com>
2022-09-14 21:43:54 +0300

    add exam002_s4

---
+++ example002_hw_sem4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11       82 -> 10           9012 -> 12
+Console.WriteLine("Input Number");
+int Number = Convert.ToInt32(Console.ReadLine());
+int temp = Number;
+int firstNum = 0;
+int GetSum(int num1)
+{
+    while (temp != 0)
+    {
+        firstNum = firstNum + (temp % 10);
+        temp = temp / 10;
+    }
+    return firstNum;
+
+}
+int sum = GetSum(Number);
+Console.WriteLine(sum);
